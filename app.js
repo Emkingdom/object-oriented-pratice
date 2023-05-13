c0cda804f41cef45f3361b2a6327da5ff9f5d954
Emmanuel Bonilla <bonillaem10@gmail.com>
2023-05-13 16:06:02 -0300

    feact: adding Vehicles class, able to create Car and Mortorcycle

---
+++ app.js
@@ -0,0 +1,57 @@
+class Vehicle {
+  constructor(make, model, year, wheels) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.numWheels = wheels;
+  }
+
+  honk() {
+    return "Beep.";
+  }
+
+  toString() {
+    const { make, model, year } = this;
+    return `The vehicle is a ${make} ${model} from ${year}`;
+  }
+}
+
+class Car extends Vehicle {
+  constructor(make, model, year, wheels = 4) {
+    super(make, model, year, wheels);
+  }
+}
+
+class Motorcycle extends Vehicle {
+  constructor(make, model, year, wheels = 2) {
+    super(make, model, year, wheels);
+  }
+
+  revEngine() {
+    return "VROOM!!!";
+  }
+}
+
+class Garage {
+  constructor(capacity) {
+    this.vehicles = [];
+    this.capacity = capacity;
+  }
+
+  add(vehicle) {
+    const { vehicles, capacity } = this;
+    let message = "";
+    if (!(vehicle instanceof Vehicle)) {
+      return "Only vehicles are allowed in here!";
+    }
+    if (capacity !== vehicles.length) {
+      vehicles.push(vehicle);
+      message = "Vehicle added!";
+    } else {
+      message = "Sorry, we're full.";
+    }
+    return message;
+  }
+}
+
+let garage = new Garage(2);
